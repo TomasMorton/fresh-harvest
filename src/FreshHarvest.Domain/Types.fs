eb654b8c10e1b4813003f36cc1f833ba92dd5017
Tomas Morton <tomas@ayudasystems.com>
2020-04-05 14:25:44 -0400

    Add some more types after consulting stakeholder

--- src/FreshHarvest.Domain/Types.fs
+++ src/FreshHarvest.Domain/Types.fs
@@ -5,20 +5,30 @@ open System
 module Seeds =
     type SunlightRequirement =
         | Full
+        | Half
         | Shade
-        | None
+
+    type SoilType =
+        | Sand
+        | Dirt
 
     type WateringInfo =
         { FreqInDays: int
           VolumeInMl: int }
 
+    type StartingInfo =
+        { IdealDate: DateTime
+          DepthInCm: int }
+
     type PlantingInfo =
         { IdealDate: DateTime
           DepthInCm: int
-          DistanceInCm: int }
+          DistanceInCm: int
+          Soil: SoilType }
 
     type Seed =
         { DaysToSprout: int
+          Starting: StartingInfo option
           Planting: PlantingInfo
           Watering: WateringInfo
           Sunlight: SunlightRequirement }
