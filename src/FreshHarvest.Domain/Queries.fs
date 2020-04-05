ab55ecece76f9e9d8e3a4f5a2c96d9722e7cf11c
Tomas Morton <tomas@ayudasystems.com>
2020-04-05 14:11:47 -0400

    Add queries for seeds

---
+++ src/FreshHarvest.Domain/Queries.fs
@@ -0,0 +1,43 @@
+module FreshHarvest.Domain.Queries
+
+open FreshHarvest.Domain.Types
+open System
+
+module SeedQueries =
+    open Seeds
+
+    let dateToExpectSprouting (plantedDate: DateTime) seed =
+        let daysToSprout =
+            seed.DaysToSprout
+            |> float
+            |> TimeSpan.FromDays
+        plantedDate.Add daysToSprout
+
+    let needsWateringToday clock (plantedDate: DateTime) seed =
+        //todo: could fail if plantedDate is before today - or just return false
+        let today = clock()
+        let daysGrown = (today - plantedDate).TotalDays |> int
+        daysGrown % seed.Watering.FreqInDays = 0
+
+    module PotSizes =
+        let isPotDeepEnough potDepth seed =
+            //TODO: maybe the depth you plant at and the depth it grows to need to be separate fields?
+            seed.Planting.DepthInCm < potDepth
+
+        let isPotWideEnough potWidth seed =
+            seed.Planting.DistanceInCm <= potWidth
+
+        let isPotLongEnough potLength seed =
+            seed.Planting.DistanceInCm <= potLength
+
+        let isPotBigEnough potLength potWidth potDepth seed =
+            isPotLongEnough potLength seed && isPotWideEnough potWidth seed && isPotDeepEnough potDepth seed
+
+        let howManySeedsFitLengthwise potLength seed =
+            potLength / seed.Planting.DistanceInCm
+
+        let howManySeedsFitWidthwise potWidth seed =
+            potWidth / seed.Planting.DistanceInCm
+
+        let howManySeedsFit potLength potWidth seed =
+            howManySeedsFitLengthwise potLength seed * howManySeedsFitWidthwise potWidth seed
